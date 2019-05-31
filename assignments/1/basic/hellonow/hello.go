d0047e7efe9a56495fa456daa594787eb0453a36
Artem Zarubin <azarubin@me.com>
2019-05-31 16:18:07 +0300

    some clean. yeah right in master ololo 'cause i can

--- assignments/1/basic/hellonow/hello.go
+++ assignments/1/basic/hellonow/hello.go
@@ -7,7 +7,8 @@ import (
 
 const ntpServer = "0.beevik-ntp.pool.ntp.org"
 
-// GetTime ...
+// GetTime returns a response from remote NTP server.
+// It contains current time and some error if occurred
 func GetTime() (time.Time, error) {
 	resp, err := ntp.Query(ntpServer)
 	return resp.Time, err
--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,6 @@
 package tools
 
-// CheckAndPanicError ...
+// CheckAndPanicError check the given error and panic if occurred some one
 func CheckAndPanicError(e error) {
 	if e != nil {
 		panic(e)
